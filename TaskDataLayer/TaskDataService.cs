60e32acd6ea6fed899b7f87bd9512e77eb4130d9
noodlss07 <lancesular@gmail.com>
2026-03-22 00:38:41 +0800

    DL/BL/MODEL Fix

---
+++ TaskDataLayer/TaskDataService.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using TaskMODEL;
+
+namespace TaskDataLayer
+{
+    public class TaskDataService
+    {
+        
+        public List<TaskModels> taskList = new List<TaskModels>();
+        
+        public void SaveTask(TaskModels newTask)
+        {
+            taskList.Add(newTask);
+        }
+
+        public List<TaskModels> GetAllTasks()
+        {
+            return taskList;
+        }
+        
+        public void UpdateTask(int index, string newName)
+        {
+            taskList[index].Name = newName;
+        }
+
+        public void DeleteAll()
+        {
+            taskList.Clear();
+        }
+    }
+}
